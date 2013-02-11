71182106b7c23fa78bdf44476c0d462bbebf232f
kongo2002 <kongo2002@googlemail.com>
2013-02-11 23:31:10 +0100

    add position related calls

--- player.php
+++ player.php
@@ -16,6 +16,34 @@ class Player
 
         return array_map($func, $cmd->fetchAll());
     }
+
+    public static function get($db, $id)
+    {
+        $cmd = $db->prepare('SELECT players.id,firstname,lastname,positions.name '.
+            'FROM players INNER JOIN positions ON position=positions.id WHERE '.
+            'players.id=:id;');
+
+        $cmd->execute(array(':id' => $id));
+
+        $player = $cmd->fetch(PDO::FETCH_NUM);
+
+        if ($player == null)
+            throw new ApiException("there is no player with ID '$id'");
+
+        return array('id' => $player[0],
+            'name' => $player[1].' '.$player[2],
+            'position' => $player[3]);
+    }
+}
+
+class Position
+{
+    public static function getAll($db)
+    {
+        $cmd = $db->query('SELECT id,name FROM positions;');
+
+        return $cmd->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
 
 class PlayerController
@@ -34,6 +62,38 @@ class PlayerController
         $db = null;
 
         return $players;
+    }
+
+    /**
+     * Get a player's detail information
+     *
+     * @url GET /player/$id
+     */
+    public function playerInfo($id)
+    {
+        $db = SafePDO::create();
+
+        $player = Player::get($db, $id);
+
+        $db = null;
+
+        return $player;
+    }
+
+    /**
+     * Get all possible positions
+     *
+     * @url GET /positions
+     */
+    public function positions()
+    {
+        $db = SafePDO::create();
+
+        $positions = Position::getAll($db);
+
+        $db = null;
+
+        return $positions;
     }
 }
 
