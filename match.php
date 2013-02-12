c713bbf72372334a4214c6a35f8d14d221a91c15
kongo2002 <kongo2002@googlemail.com>
2013-02-12 23:56:38 +0100

    add participating players to match call

--- match.php
+++ match.php
@@ -4,6 +4,7 @@ class Match
 {
     public static function get($db, $id)
     {
+        # get match information
         $cmd = $db->prepare('SELECT date,teams.name,sum(matchevents.goals) as goals,'.
             'opponent_goals,homegame FROM matches '.
             'INNER JOIN teams ON teams.id=opponent '.
@@ -12,15 +13,46 @@ class Match
 
         $cmd->execute(array(':id' => $id));
 
+        # fetch first result
         $match = $cmd->fetch(PDO::FETCH_ASSOC);
 
         if ($match == null || $match['date'] == null)
             throw new ApiException("there is no match with ID '$id'");
 
+        # correct goals/homegame data types
         $match['goals'] = intval($match['goals']);
         $match['homegame'] = $match['homegame'] > 0 ? true : false;
 
         return $match;
+    }
+
+    public static function getMatchPlayers($db, $id)
+    {
+        # get participating players
+        $cmd = $db->prepare('SELECT player,concat(firstname," ",lastname) as name,'.
+            'substitution FROM matchup '.
+            'INNER JOIN players ON players.id=matchup.player '.
+            'WHERE matchup.match=:id;');
+
+        $cmd->execute(array(':id' => $id));
+
+        $starters = array();
+        $subs = array();
+
+        # fetch all players
+        foreach ($cmd->fetchAll(PDO::FETCH_NUM) as $player)
+        {
+            $sub = $player[2];
+
+            $p = array('id' => $player[0], 'name' => $player[1]);
+
+            if ($sub != null && $sub > 0)
+                $subs[] = $p;
+            else
+                $starters[] = $p;
+        }
+
+        return array($starters, $subs);
     }
 }
 
@@ -33,7 +65,16 @@ class MatchController
      */
     public function getMatch($id)
     {
-        return Match::get($this->database, $id);
+        $db = $this->database;
+
+        $match = Match::get($db, $id);
+
+        list($starters, $subs) = Match::getMatchPlayers($db, $id);
+
+        $match['starters'] = $starters;
+        $match['substitutes'] = $subs;
+
+        return $match;
     }
 }
 
