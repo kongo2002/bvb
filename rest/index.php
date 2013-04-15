946a908535b7cc0aee88eb770de38f9005ceb788
kongo2002 <kongo2002@googlemail.com>
2013-04-15 22:25:33 +0200

    cleanup

--- rest/index.php
+++ rest/index.php
@@ -1,23 +1,23 @@
 <?php
 
-// load server logic
+# load server logic
 require_once('server.php');
 
-// load controllers
+# load controllers
 require_once('base.php');
 require_once('user.php');
 require_once('player.php');
 require_once('match.php');
 require_once('team.php');
 
-// don't load our classes unless we use them
+# don't load our classes unless we use them
 spl_autoload_register();
 
-// 'debug' or 'production'
+# 'debug' or 'production'
 $mode = 'debug';
 $server = new RestServer($mode, 'BVB_REST');
 
-// uncomment to clear the cache in production mode
+# uncomment to clear the cache in production mode
 $server->refreshCache();
 
 $server->addClass('BaseController', '/');
