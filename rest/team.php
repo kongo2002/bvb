0efa85088abc53390e5bcd26464125457a03d4d9
kongo2002 <kongo2002@googlemail.com>
2013-04-19 00:32:04 +0200

    more team related API functions

--- rest/team.php
+++ rest/team.php
@@ -30,13 +30,21 @@ class Team
 
     public static function add($db, $team)
     {
-        if (!$teamName)
+        if (!$team)
             throw new ApiException('invalid team name given');
 
         $cmd = $db->prepare('INSERT INTO teams (name) VALUES (:name);');
         $cmd->execute(array(':name' => $team));
 
         return $db->lastInsertId();
+    }
+
+    public static function remove($db, $teamId)
+    {
+        $cmd = $db->prepare('DELETE FROM teams WHERE id = :id;');
+        $cmd->execute(array(':id' => $teamId));
+
+        return $cmd->rowCount() == 1;
     }
 
     public static function getList($db)
@@ -71,6 +79,44 @@ class TeamController
         $db = $this->database;
 
         return Team::getList($db);
+    }
+
+    /**
+     * Add a new team
+     *
+     * @url POST /team
+     */
+    public function addTeam($data)
+    {
+        $db = $this->database;
+
+        if (!$data || !isset($data->name))
+            throw new ApiException('no or invalid team name given');
+
+        $team = $data->name;
+
+        if (!Team::exists($db, $team))
+        {
+            $id = Team::add($db, $team);
+            return array('id' => $id);
+        }
+
+        throw new ApiException("team '$team' already exists");
+    }
+
+    /**
+     * Delete the team with the specified ID
+     *
+     * @url DELETE /team/$id
+     */
+    public function deleteTeam($id)
+    {
+        $db = $this->database;
+
+        if (!Team::remove($db, $id))
+            throw new ApiException('there is no team with ID ' . $id);
+
+        return true;
     }
 }
 
