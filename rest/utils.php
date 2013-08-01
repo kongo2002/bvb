cdc7122193bc460f617751c91dfce9189cb56f8f
kongo2002 <kongo2002@googlemail.com>
2013-08-01 22:28:25 +0200

    Utils class somehow got lost...

---
+++ rest/utils.php
@@ -0,0 +1,16 @@
+<?php
+
+class Utils
+{
+    public static function isValidDate($input)
+    {
+        $date = date_parse($input);
+
+        return $date &&
+            $date['day'] > 0 &&
+            $date['month'] > 0 &&
+            $date['year'] > 0 &&
+            count($date['errors']) < 1;
+    }
+}
+?>
